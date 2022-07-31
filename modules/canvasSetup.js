61cd4594ba7b96fcb00e411b223526a943e170c7
lluvz <2572502109@qq.com>
2022-07-31 20:30:06 +0800

    error

--- modules/canvasSetup.js
+++ modules/canvasSetup.js
@@ -2,7 +2,7 @@
 let scene=new THREE.Scene();
 let camera=new THREE.PerspectiveCamera(75,w/h,0.1,1000);
 let renderer=new THREE.WebGLRenderer();
-renderer.setSize(w,h);
+renderer.setSize(window.innerWidth,window.innerHeight);
 document.body.appendChild(renderer.domElement);
 //test
 const geometry = new THREE.BoxGeometry( 70, 70, 70 );
--- modules/outer.js
+++ modules/outer.js
@@ -1,9 +1,7 @@
-
 let t='hello world';
 let h=screen.height;
 let w=screen.width;
 let s=w/30;
-
 //vconsole
 let vc=true;
 let vconsole=new window.VConsole();
@@ -15,22 +13,22 @@ let icons={
         let i=this.left.push(document.createElement('img'))-1;
         this.left[i].src=iconSrc[name];
 
-        this.left[i].width=this.left[i].height=s;
+        this.left[i].width=this.left[i].height=50;
         document.body.appendChild(this.left[i]);
         this.left[i].style.position='fixed';
         this.left[i].style.top=0;
-        this.left[i].style.left=i*s+'px';
+        this.left[i].style.left=i*50+'px';
         this.left[i].onclick=click;
         this.left[i].alt=name;
     },
     rightAdd:function(name,click){//name need to be consistent with the src, which is in the imgs/icon and is a png img.
         let i=this.right.push(document.createElement('img'))-1;
         this.right[i].src=iconSrc[name];
-        this.right[i].width=this.right[i].height=s;
+        this.right[i].width=this.right[i].height=50;
         document.body.appendChild(this.right[i]);
         this.right[i].style.position='fixed';
         this.right[i].style.top=0;
-        this.right[i].style.right=(i)*s+'px';
+        this.right[i].style.right=(i)*50+'px';
         this.right[i].onclick=click;
         this.right[i].alt=name;
     }
@@ -39,12 +37,14 @@ let icons={
 let fullscreen=false;
 function fs(){
     if(fullscreen==false){
+        renderer.setSize(w,h)
         document.body.requestFullscreen();
         fullscreen=true;
     }
     else{
         document.exitFullscreen();
         fullscreen=false;
+        renderer.setSize(window.innerWidth,window.innerHeight)
     }
 }
 icons.leftAdd('fullscreen',fs);
@@ -58,6 +58,7 @@ icons.rightAdd('set',sts);
 let help=true;
 function hp(){
     console.log(t)
+    renderer.setSize(window.innerWidth,window.innerHeight)
 }
 icons.leftAdd('help',hp);
 //play
@@ -67,9 +68,16 @@ function pl(){
 }
 icons.rightAdd('play',pl);
 
-let playContainer=document.getElementById('playContainer');
+let play=document.getElementById('play');
+let playOption=document.getElementById('playOption');
+let cancelPlay=document.getElementById('cancelPlay')
 playContainer.onclick=function(){
-    console.log(t)
+    play.style.display='none';
+    playOption.style.display='block';
+}
+cancelPlay.onclick=function(){
+    play.style.display='block';
+    playOption.style.display='none';
 }
 
 
